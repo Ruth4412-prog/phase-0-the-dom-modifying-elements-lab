2bde3147da786f7b3d4be70694353abd67eccf26
Ruth4412-prog <kipchauijepkaa@gmail.com>
2022-08-15 02:04:41 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById(`main`).remove();
+let newHeader = document.createElement(`h1`)
+document.body.appendChild(newHeader)
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = `Ruth is the champion`;
