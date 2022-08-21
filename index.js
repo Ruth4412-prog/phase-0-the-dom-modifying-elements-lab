9df94021813da6b9130915e1b66133cde4249423
Ruth Jepkalal <kipchauijepkaa@gmail.com>
2022-08-21 09:31:44 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Write your code here!
 document.getElementById(`main`).remove();
+
 let newHeader = document.createElement(`h1`)
+
 document.body.appendChild(newHeader)
+
 newHeader.setAttribute("id","victory")
+
 newHeader.innerHTML = `Ruth is the champion`;
